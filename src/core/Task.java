5d8d44c443abde2ce1a9efe782cf2b8f3810c93c
DeantoPrivate <lqopl@mail.ru>
2014-06-19 21:30:54 +0400

    add some

--- src/core/Task.java
+++ src/core/Task.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 public class Task {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.TABLE)
     @Column(name="task_id")
     private int id;
 
